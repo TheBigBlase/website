5210bdc66c4256408c72cff3dc48304ad1b542db
thebigblase <thebigblase@gmail.com>
2020-11-19 20:53:55 +0100

    aaaaaaaaaaa

--- routes/lamp.js
+++ routes/lamp.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('lamp', { title: 'you are speed' } );
 });
 
+router.post('/', function(req, res, next) {
+	const method = req.body.method;
+	const redValue = req.body.red;
+	const greenValue = req.body.green;
+	const blueValue = req.body.blue;
+	if(method == 'rainbow'){
+		asiox.post('127.0.0.1:9003/rainbow', { 
+			})
+		.then(res => {
+			console.log(res.statusCode)})
+		.catch(error => {
+			console.error(error)});
+	}
+	else if(method == 'wipe'){
+		asiox.post('127.0.0.1:9003/wipe', { 
+			red: redValue,
+			green: greenValue,
+			red: redValue
+			})
+		.then(res => {
+			console.log(res.statusCode)})
+		.catch(error => {
+			console.error(error)});
+	}
+	else if(method == 'clear'){
+		asiox.post('127.0.0.1:9003/', { 
+			})
+		.then(res => {
+			console.log(res.statusCode)})
+		.catch(error => {
+			console.error(error)});
+	}
+	else {
+		res.rendre('error', { title: '404 you dumb dumb'});
+	}
+	});
+	
 module.exports = router;
