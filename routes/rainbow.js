118e7f15a5530f22e95663d3ba2c67c6f828bb97
thebigblase <thebigblase@gmail.com>
2020-11-21 22:13:37 +0100

    lol

--- routes/rainbow.js
+++ routes/rainbow.js
@@ -6,8 +6,8 @@ const axios = require('axios');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 			console.log('fuck you sample');
-			axios.post('127.0.0.1:9003/rainbow', {
-			})
+			axios.post('127.0.0.1:9003/rainbow' 
+			)
 		.then(res => {
 			console.log(res.statusCode)})
 		.catch(error => {
