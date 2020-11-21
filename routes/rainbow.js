58aaf261932de2ed1d72eb2539908ff817571bc6
thebigblase <thebigblase@gmail.com>
2020-11-21 22:44:00 +0100

    lol

--- routes/rainbow.js
+++ routes/rainbow.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-
+const { exec } = require("child_process");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-		console.log('fuck you sample');
-		axios.post('127.0.0.1:9003/rainbow');
+	console.log('fuck you sample');
+	exec("curl -X POST localhost:9003",(error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 });
 	
 module.exports = router;
